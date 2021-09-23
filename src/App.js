155302142d28dc996619edbc3c985bc85f0f562d
elangsamudra <elangsamudra48@gmail.com>
2021-09-23 14:57:16 +0700

    Offline Info dan Manifest

--- src/App.js
+++ src/App.js
@@ -6,28 +6,52 @@ import Clients from "./components/Clients";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Offline from "./components/Offline";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [offlineStatus, setOfflineStatus] = useState(!navigator.onLine);
 
-  useEffect(function () {
-    (async function () {
-      const response = await fetch(
-        "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            accept: "application/json",
-            "x-api-key": process.env.REACT_APP_APIKEY,
-          },
-        }
-      );
-      const { nodes } = await response.json();
-      setItems(nodes);
-    })();
-  }, []);
+  function handleOfflineStatus() {
+    setOfflineStatus(!navigator.onLine);
+  }
+
+  useEffect(
+    function () {
+      (async function () {
+        const response = await fetch(
+          "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+              "x-api-key": process.env.REACT_APP_APIKEY,
+            },
+          }
+        );
+        const { nodes } = await response.json();
+        setItems(nodes);
+
+        const script = document.createElement("script");
+        script.src = "/carousel.js";
+        script.async = false;
+        document.body.appendChild(script);
+      })();
+
+      handleOfflineStatus();
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
+
+      return function () {
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
+      };
+    },
+    [offlineStatus]
+  );
   return (
     <>
+      {offlineStatus && <Offline />}
       <Header />
       <Hero />
       <Browse />
